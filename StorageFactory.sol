4f7408d4b2f4e983206718de75973b8450a9ec01
Afoma Orji <antoinetteorji@gmail.com>
2024-11-20 15:24:21 +0100

    Update StorageFactory.sol

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
 
+import {SimpleStorage} from "contracts/SimpleStorage.sol";
+
+contract StorageFactory{
+
+    SimpleStorage public simpleStorage;
+
+    function createSimpleStorage() public{
+        simpleStorage = new SimpleStorage();
+    }
+}

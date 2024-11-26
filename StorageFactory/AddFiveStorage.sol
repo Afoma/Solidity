3c2b85d8b93baf90678f79579378798fbcccd515
Afoma Orji <antoinetteorji@gmail.com>
2024-11-26 20:31:58 +0100

    Update AddFiveStorage.sol

--- StorageFactory/AddFiveStorage.sol
+++ StorageFactory/AddFiveStorage.sol
@@ -1,1 +1,8 @@
-
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+import {SimpleStorage} from "contracts/SimpleStorage.sol";
+contract AddFiveStorage is SimpleStorage{
+    function store (uint256 _newNumber) public override {
+        numbers = _newNumber+5;
+    }
+}

1a22f24acd2f09ec33d6dc51dc7ef12ffac8ff29
Afoma Orji <antoinetteorji@gmail.com>
2024-11-27 14:39:16 +0100

    Update FundMe.sol

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -1,1 +1,10 @@
-
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+contract FundMe{
+    // Get Funds
+    // Withdraw funds
+    // Set minimum funding vaLue in USD
+    function fund() public payable{
+        require(msg.value > 1e18, "didn't send enough ETH");
+    }
+}

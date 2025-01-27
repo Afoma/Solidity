2c8ef4a7b94246e2b44110e770110ccc046069be
Afoma Orji <antoinetteorji@gmail.com>
2025-01-27 07:16:12 -0800

    Added inheritance property to AddFiveStorage.sol

--- StorageFactory/AddFiveStorage.sol
+++ StorageFactory/AddFiveStorage.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.24;
 import {SimpleStorage} from "contracts/SimpleStorage.sol";
 contract AddFiveStorage is SimpleStorage{
+// the `is` keyword lets `contract AddFiveStorage` inherit all the properties of `contract SimpleStorage`
+// with this you can carry out operations on SimpleStorage's ABI
     function store (uint256 _newNumber) public override {
         numbers = _newNumber+5;
     }

aac79c90907d789b0ac481a52f1bbbadb4ccd9d7
Afoma Orji <antoinetteorji@gmail.com>
2025-02-10 12:24:32 -0800

    Imported interface library from Chainlink's GitHub

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -1,16 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe{
     // Get Funds
     // Withdraw funds
     // Set minimum funding vaLue in USD
+    uint256 public minUsd = 5;
 
     function fund() public payable{
     // Allow users to send $
     // Have minimum $ sent
     // How do we send ETH to this contract
     
-        require(msg.value > 1e18, "didn't send enough ETH");
+        require(msg.value >= minUsd, "didn't send enough ETH");
+    }
+    function getFunds() public{
+        // Address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // ABI
+    }
+    function getConversionRate() public{
+
+    }
+    function getVersion() public view returns(uint256){
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
     }
 }

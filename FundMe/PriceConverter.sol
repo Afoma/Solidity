fa59427b66fdf5bfcd7f4fc8b50185d304e1f9d8
Afoma Orji <antoinetteorji@gmail.com>
2025-03-14 07:33:49 -0700

    Create PriceConverter.sol

---
+++ FundMe/PriceConverter.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+using PriceConverter for uint256;
+
+library PriceConverter{
+        function getPrice() internal view returns(uint256){
+        AggregatorV3Interface dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int256 answer, , ,) = dataFeed.latestRoundData();
+        return uint256(answer * 1e10);
+    }
+        function getConversionRate(uint256 ethAmount) internal view returns(uint256){
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        return ethAmountInUsd;
+    }
+        function getDecimals() internal view returns(uint8){
+        AggregatorV3Interface dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return dataFeed.decimals();
+    }
+    
+    function getVersion() public view returns(uint256){
+        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    }
+
+}

565ae3891defeffde1cb0209f2e511d243f38a76
Afoma Orji <antoinetteorji@gmail.com>
2025-03-19 07:46:48 -0700

    Update FundMe.sol

--- FundMe/FundMe.sol
+++ FundMe/FundMe.sol
@@ -1,21 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 import {PriceConverter} from "./PriceConverter.sol";
+
+error NotOwner();
+
 contract FundMe{
-    uint256 public minUsd = 5e18;
+    uint256 public constant MINIMUM_USD = 5e18;
+    // 21,415 gas with constant keyword
+    // 23,515 gas without 
+    // 21,415 * 141000000000 = $9.058545
+    // 23,515 * 141000000000 = $9.946845
     address[] public funders;
     mapping (address =>uint256) public addressToAmountFunded;
 
     using PriceConverter for uint256;
 
-    address public owner;
+    address public immutable i_owner;
+    // 21,508 gas immutable
+    // 23,644 gas without immutable
 
     constructor(){
-        owner = msg.sender;
+        i_owner = msg.sender;
     }
 
     function fund() public payable{
-        require(msg.value.getConversionRate() >= minUsd, "not enough ETH");
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "not enough ETH");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
@@ -31,8 +40,11 @@ contract FundMe{
     }
 
     modifier onlyOwner(){
-        require(msg.sender == owner, "Sender not owner");
+        // require(msg.sender == i_owner, "Sender not owner");
+        // the method above can be written instead of the if statement
+        // either of them works
         _;
+        if (msg.sender != i_owner) {revert NotOwner();}
     }
 
 }
